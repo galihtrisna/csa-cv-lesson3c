2277a271f17078370b2e955f5eee6d761ea454c9
Galih Trisna <galihtrisna666@gmail.com>
2026-03-09 17:35:14 +0700

    first commit

---
+++ src/main/java/com/codedotorg/AppLogic.java
@@ -0,0 +1,79 @@
+package com.codedotorg;
+
+public class AppLogic {
+
+    /** The pin to unlock the app */
+    private String pin;
+
+    /** The pin the user has provided */
+    private String user;
+
+    /**
+     * Constructor for the AppLogic class.
+     * Initializes the pin with a random value and user with an empty string.
+     */
+    public AppLogic() {
+        pin = createRandomPin();
+        user = "";
+    }
+
+    /**
+     * Creates a user PIN based on the predicted class.
+     * @param predictedClass the predicted class from the machine learning model
+     * @return the user PIN as a string
+     */
+    public String createUserPin(String predictedClass) {
+        
+        if (!predictedClass.contains("neutral") && user.length() < 4) {
+            String number = predictedClass.substring(0, 1);
+            user += number;
+        }
+        return user;
+    }
+
+    /**
+     * Checks if the length of the user's PIN is equal to 4.
+     * @return true if the length of the user's PIN is equal to 4, false otherwise.
+     */
+    public boolean checkPinLength() {
+        
+        return user.length() == 4;
+    }
+
+    /**
+     * Returns the status of the user's input PIN.
+     * @param userPin the PIN entered by the user
+     * @return a string indicating whether the PIN is correct or not
+     */
+    public String getPinStatus(String userPin) {
+        
+        if (userPin.equals(pin)) {
+            return "Unlocked Successfully!";
+        } else {
+            return "Incorrect PIN. Try Again.";
+        }
+    }
+    
+    /**
+     * Resets the logic of the application by generating
+     * a new random PIN and clearing the user field.
+     */
+    public void resetLogic() {
+        pin = createRandomPin();
+        user = "";
+    }
+
+    /**
+     * Generates a random 4-digit PIN number.
+     * @return the generated PIN number as a string.
+     */
+    private String createRandomPin() {
+        String randomPin = "";
+        for (int i = 0; i < 4; i++) {
+            int randomNum = (int) (Math.random() * 9);
+            randomPin += randomNum;
+        }
+        return randomPin;
+    }
+
+}
